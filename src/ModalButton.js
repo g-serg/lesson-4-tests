19de297ca1f5bf67c4fd85eac46af4dad64dce71
g-serg <mr-sergey2004@yandex.ru>
2017-11-15 00:54:05 +0300

    all tests done

--- src/ModalButton.js
+++ src/ModalButton.js
@@ -1,8 +1,45 @@
-import {Component} from 'react';
+import React, {Component} from 'react';
+import Modal from './Modal';
 
 class ModalButton extends Component {
+  state = {isModalShow: false};
+
+  static displayName = 'Modal button';
+
+  hideModal = () => {
+    this.setState({isModalShow: false});
+  };
+
+  showModal = () => {
+    this.setState({isModalShow: true});
+  };
+
   render() {
-    return null;
+    const {isModalShow} = this.state;
+
+    return (
+      <div>
+        <button onClick={this.showModal}>
+          ShowModal!
+        </button>
+        {isModalShow && (
+          <Modal>
+            <div className="modal">
+              <div className="modal__fog">
+                <div className="modal__body">
+                  <h1> Modal window!</h1>
+                  <button
+                    onClick={this.hideModal}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>,
+          </Modal>
+        )}
+      </div>
+    );
   }
 }
 
--- src/Switcher.js
+++ src/Switcher.js
@@ -1,12 +1,60 @@
-import {Component} from 'react';
+import React, {Component} from 'react';
 import './Switcher.css';
 
 // Для работы этой компоненты нужно использовать методы React.Children.toArray
 // а так же работать с child.type.name и child.type.displayName
 
 class Switcher extends Component {
+  state = {
+    selectedChild: 0
+  };
+
+  renderTitle = (el, index) => {
+    const {type: {displayName, name}} = el;
+
+    return (
+      <li
+        className="component-list__name"
+        data-id={index}
+        onClick={this.handleChangeChild}
+      >
+        {displayName || name}
+      </li>
+    );
+  };
+
+  handleChangeChild = event => {
+    const dataId = +event.target.getAttribute(
+      'data-id'
+    );
+    this.setState({selectedChild: dataId});
+  };
+
   render() {
-    return null;
+    const {children} = this.props;
+    const {selectedChild} = this.state;
+
+    return (
+      <div>
+        <nav>
+          <ul className="component-list">
+            {React.Children.map(
+              children,
+              this.renderTitle
+            )}
+          </ul>
+        </nav>
+        <hr />
+        <div className="component-wrapper">
+          {React.Children
+            .toArray(children)
+            .filter(
+              (item, index) =>
+                index === selectedChild
+            )}
+        </div>
+      </div>
+    );
   }
 }
 
